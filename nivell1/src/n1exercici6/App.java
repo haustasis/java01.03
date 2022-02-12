467fd79e22b513e55f6560189c9f557e59ab46fc
TARISO <toni.ariso@gmail.com>
2022-02-12 19:26:12 +0100

    clean ex5 perfect

--- nivell1/src/n1exercici6/App.java
+++ nivell1/src/n1exercici6/App.java
@@ -1,7 +1,27 @@
 package n1exercici6;
-/*
-Exercici 6. Crea un objecte buit LinkedList<Integer>. Utilitzant un iterador ListIterator,
-afegeixi valors sencers a la llista inserint-los sempre a la meitat d'aquesta.
- */
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class App {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>() ;
+        getIterator(list).add(1);
+        getIterator(list).add(2);
+        getIterator(list).add(3);
+        getIterator(list).add(4);
+        getIterator(list).add(5);
+        getIterator(list).add(6);
+        getIterator(list).add(7);
+        getIterator(list).add(8);
+
+        for(Integer i : list) {
+            System.out.println(i);
+        }
+    }
+
+    private static ListIterator<Integer> getIterator(LinkedList<Integer> list) {
+        return list.listIterator( list.size() / 2 );
+    }
 }
+
