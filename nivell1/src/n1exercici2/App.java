54c7007a38e9fd2147b713844d184b91261a2f16
TARISO <toni.ariso@gmail.com>
2022-02-12 16:39:19 +0100

    clean ex2

--- nivell1/src/n1exercici2/App.java
+++ nivell1/src/n1exercici2/App.java
@@ -1,7 +1,46 @@
 package n1exercici2;
-/*
-Exercici 2. Asseguri's que si convertim el Contenidor utilitzat en el punt anterior a un Contenidor HashSet,
-aquest no permet duplicats. Verifica-ho.
- */
+
+import java.util.HashSet;
+
 public class App {
+    public static void main(String[] args) {
+
+        HashSet<String> year = new HashSet<String>();
+        year.add("Gener");
+        year.add("Gener");
+        year.add("Gener");
+        year.add("Febrer");
+        year.add("Febrer");
+        year.add("Febrer");
+        year.add("Març");
+        year.add("Març");
+        year.add("Març");
+        year.add("Abril");
+        year.add("Abril");
+        year.add("Abril");
+        year.add("Maig");
+        year.add("Maig");
+        year.add("Juny");
+        year.add("Juny");
+        year.add("Juny");
+        year.add("Juliol");
+        year.add("Juliol");
+        year.add("Agost");
+        year.add("Agost");
+        year.add("Setembre");
+        year.add("Setembre");
+        year.add("Setembre");
+        year.add("Novembre");
+        year.add("Novembre");
+        year.add("Novembre");
+        year.add("Desembre");
+        year.add("Desembre");
+        year.add("Desembre");
+
+
+        for (String str : year) {
+            System.out.println(str);
+        }
+
+    }
 }
